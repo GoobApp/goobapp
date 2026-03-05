3324497b472e50cd6c8e5523f099e167955a61d1
Precontation <precontation@proton.me>
2026-03-05 12:41:36 -0800

    Update Socket.tsx

--- src/utils/Socket.tsx
+++ src/utils/Socket.tsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const URL = import.meta.env.PROD
-  ? import.meta.env.IS_NETLIFY ? "goobapp-backend.netlify.app" : "https://goobapp-server.koyeb.app"
+  ? import.meta.env.VITE_IS_NETLIFY ? "goobapp-backend.netlify.app" : "https://goobapp-server.koyeb.app"
   : "http://localhost:3000"; // If import.meta.env.PROD is true, then you are in production. Otherwise just use localhost
 
 export const SERVER_URL = URL;
