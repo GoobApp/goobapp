54c5e9f88a0957b35a6c63a8cd7531aa609fd131
Precontation <precontation@proton.me>
2026-03-16 16:53:01 -0700

    no special treatement for netlify

--- src/utils/Socket.tsx
+++ src/utils/Socket.tsx
@@ -1,19 +1,13 @@
 import { io } from "socket.io-client";
 
-const isNetlify = import.meta.env.VITE_IS_NETLIFY === "true";
-
-// If on Netlify, use a relative path to trigger the netlify.toml proxy
-// Otherwise, use the direct Render URL (for Cloudflare Pages or local dev)
 const URL = import.meta.env.PROD
-  ? isNetlify
-    ? "/"
-    : "https://goobapp-backend-tsil.onrender.com"
+  ? "https://goobapp-backend-tsil.onrender.com"
   : "http://localhost:3000";
 
 export const SERVER_URL = URL;
 
 export const socket = io(URL, {
   autoConnect: false,
-  path: isNetlify ? "/api/socket.io" : "/socket.io",
+  path: "/socket.io",
   transports: ["polling", "websocket"],
 });
